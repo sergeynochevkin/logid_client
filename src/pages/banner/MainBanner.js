d76362378c5496bcf172f099244dc55016430f5e
sergeynochevkin <sergey.nochevkin@gmail.com>
2022-12-24 02:20:29 +0300

    banner

--- src/pages/banner/MainBanner.js
+++ src/pages/banner/MainBanner.js
@@ -12,10 +12,10 @@ const MainBanner = observer(() => {
 
     return (
         <div className={'main_banner_container'} style={{ backgroundImage: `url(${image})`, width: '100%', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPositionX: 'center' }}>
-            <div className={'main_nbanner_half_1_container'}>
+            <div className={'main_banner_half_1_container'}>
                 <div className={Setting.app_theme === 'light' ? 'main_banner_slogan' : 'main_banner_slogan main_banner_slogan_dark'}>{SetNativeTranslate(Translate.language, {}, 'main_slogan')}</div>
             </div>
-            <div className={Setting.app_theme === 'light' ? 'main_nbanner_half_2_container' : 'main_nbanner_half_2_container dark'}>
+            <div className={Setting.app_theme === 'light' ? 'main_banner_half_2_container' : 'main_banner_half_2_container dark'}>
                 <BannerActionContent />
             </div>
         </div>
