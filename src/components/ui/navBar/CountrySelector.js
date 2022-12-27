c414f1d299994d694df264839438157d0e4ce5a6
sergeynochevkin <sergey.nochevkin@gmail.com>
2022-12-27 22:37:16 +0300

    last for today

--- src/components/ui/navBar/CountrySelector.js
+++ src/components/ui/navBar/CountrySelector.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import { observer } from 'mobx-react-lite'
+import React, { useContext } from 'react'
+import { AdressContext } from '../../..'
 
-const CountrySelector = () => {
+const CountrySelector = observer(() => {
+  const { Adress } = useContext(AdressContext)
+
   return (
     <div>CountrySelector</div>
   )
-}
+})
 
 export default CountrySelector
---
+++ src/components/ui/navBar/CountrySelectorItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CountrySelectorItem = () => {
+    return (
+        <div>CountrySelectorItem</div>
+    )
+}
+
+export default CountrySelectorItem
