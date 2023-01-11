e99c2ebedfb0ed1624a7dd100b50da70bcd66c46
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-01-12 02:58:10 +0300

    legality

--- src/pages/footer/FooterSection.js
+++ src/pages/footer/FooterSection.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import { observer } from 'mobx-react-lite'
+import React, { useContext } from 'react'
+import { AdressContext } from '../..'
 import FooterItem from './FooterItem'
 
-const FooterSection = ({ section, items, setModalActive, setAgreement}) => {
+const FooterSection = observer(({ section, items, setModalActive, setAgreement }) => {
+    const { Adress } = useContext(AdressContext)
+
     return (
-        <div className={section.class}>
-            {items.map(item => <FooterItem item={item} key={item.id}  setModalActive = {setModalActive} setAgreement={setAgreement}/>)}
-        </div>
+        <>
+            {Adress.country.value === 'russia' ?
+                <div className={section.class}>
+                    {items.filter(el => el.id !== 5 && el.id !== 6).map(item => <FooterItem item={item} key={item.id} setModalActive={setModalActive} setAgreement={setAgreement} />)}
+                </div>
+                :
+                <div className={section.class}>
+                    {items.filter(el => el.id !== 4 && el.id !== 5 && el.id !== 6).map(item => <FooterItem item={item} key={item.id} setModalActive={setModalActive} setAgreement={setAgreement} />)}
+                </div>
+            }
+
+        </>
+
     )
-}
+})
 
 export default FooterSection
