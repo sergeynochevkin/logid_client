d2b4a56dd123958fbdaca06312c558a833f2f22f
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-01-14 20:14:40 +0300

    cok

--- src/pages/nav/NavBar.js
+++ src/pages/nav/NavBar.js
@@ -91,7 +91,7 @@ const NavBar = observer(() => {
               user.setUser({});
               UserInfo.setUserInfo({})
               localStorage.clear()
-              localStorage.setItem('cookies_accepted', true)
+              localStorage.setItem('cookies_accepted', false)
             }}>{SetNativeTranslate(Translate.language, {}, 'sign_out')}</div> :
           <div className='nav_bar_item' onClick={() =>
             navigate(LOGIN_ROUTE)}>{SetNativeTranslate(Translate.language, {}, 'sign_in')}</div>
