4e4769c8148824ae8dca1e7b44f54dd850f1a51b
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-02-19 16:06:44 +0300

    rate

--- src/pages/main/AdminConsoleItem.js
+++ src/pages/main/AdminConsoleItem.js
@@ -1,17 +1,25 @@
 import { observer } from 'mobx-react-lite'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { SettingContext } from '../..'
 
 const AdminConsoleItem = observer(({ plan, currentRate, comment }) => {
 
     const { Setting } = useContext(SettingContext)
-    let rate = `-2deg`
+
+    const [rate, setRate] = useState(0)
+    const [field, setField] = useState(99)
+
+    // make icreament
+    useEffect(() => {
+        setRate(currentRate / plan * 100)
+        setField(99 - currentRate / plan * 100)
+    }, [])
 
     return (
         <div className='admin_console_item'>
             <div className='admin_console_progress'>
                 <div className='admin_console_progress_circle'>
-                    <div className='admin_console_progress_segment' style={{ transform: `rotate(270deg) skew(${rate})` }}></div>
+                    <div className='admin_console_progress_segment' style={{ background: `conic-gradient(rgba(90, 90, 90, 0.792) ${rate}%, 1%,rgba(194, 194, 194, 0.822)  ${field}%)` }}></div>
                     <div className={`admin_console_progress_inner ${Setting.app_theme}`}></div>
                     <div className='admin_console_level_circle_value'>{currentRate}</div>
                     <div className='admin_console_level_circle_value_comment'>{comment}</div>
