b89fd0c00f2d8aad8c1cdeb35f9efbb1e5705ea4
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-02-20 16:04:05 +0300

    adm

--- src/Fetcher.js
+++ src/Fetcher.js
@@ -280,6 +280,18 @@ const Fetcher = observer(() => {
     }, [fetcher.account_user_info])
 
     //management:
+
+    useEffect(() => {
+        if (user.user.role === 'admin') {
+            setInterval(() => {
+                fetcher.setManagementUsers(true)
+                fetcher.setManagementOrders(true)
+                fetcher.setManagementTransports(true)
+            }, 10000);
+            clearInterval()
+        }
+    }, [])
+
     //users
     useEffect(() => {
         async function fetch() {
