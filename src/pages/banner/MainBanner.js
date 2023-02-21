072236d2bfa36f2dcf1a7ad16f00306805f87a4a
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-02-21 13:59:33 +0300

    target

--- src/pages/banner/MainBanner.js
+++ src/pages/banner/MainBanner.js
@@ -1,22 +1,26 @@
 import { observer } from 'mobx-react-lite';
 import React, { useContext } from 'react'
 import { SettingContext, TranslateContext } from '../..';
-import image from '../../assets/logistics.jpg';
+import logistics from '../../assets/logistics.jpg';
+import courier from '../../assets/courier.jpg';
 import { SetNativeTranslate } from '../../modules/SetNativeTranslate';
 import BannerActionContent from './BannerActionContent';
 
 
-const MainBanner = observer(({callRequested, setCallRequested}) => {
+const MainBanner = observer(({ callRequested, setCallRequested }) => {
     const { Setting } = useContext(SettingContext)
     const { Translate } = useContext(TranslateContext)
 
+    const queryParams = new URLSearchParams(window.location.search)
+    const target = queryParams.get("target")
+
     return (
-        <div className={'main_banner_container'} style={{ backgroundImage: `url(${image})`, width: '100%', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPositionX: 'center' }}>
+        <div className={'main_banner_container'} style={{ backgroundImage: `url(${target !== 'courier' ? logistics : courier})`, width: '100%', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPositionX: 'center' }}>
             <div className={'main_banner_half_1_container'}>
                 <div className={Setting.app_theme === 'light' ? 'main_banner_slogan' : 'main_banner_slogan main_banner_slogan_dark'}>{SetNativeTranslate(Translate.language, {}, 'main_slogan')}</div>
             </div>
             <div className={Setting.app_theme === 'light' ? 'main_banner_half_2_container' : 'main_banner_half_2_container dark'}>
-                <BannerActionContent callRequested = {callRequested} setCallRequested={setCallRequested}/>
+                <BannerActionContent callRequested={callRequested} setCallRequested={setCallRequested} />
             </div>
         </div>
     )
