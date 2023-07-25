a7a970bc50f779dbe3d522aad3935b8ce3eb6e07
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-07-25 23:55:07 +0300

    transport

--- src/pages/main/AdTransportItem.js
+++ src/pages/main/AdTransportItem.js
@@ -3,6 +3,8 @@ import React, { useContext, useEffect, useState } from 'react'
 import { AdContext, SettingContext, TranslateContext, UserContext } from '../..'
 import { SetNativeTranslate } from '../../modules/SetNativeTranslate'
 import Modal from '../../components/ui/modal/Modal'
+import { CardEquipment } from '../../components/ui/card/CardEquipment'
+import { EquipmentRow } from '../../components/ui/card/EquipmentRow'
 
 const AdTransportItem = observer(({ transport, transports }) => {
     const { Setting } = useContext(SettingContext)
@@ -55,11 +57,24 @@ const AdTransportItem = observer(({ transport, transports }) => {
                 </div>
             </div>
 
-            <div className='ad_transport_item_ad_header_container'>{`${SetNativeTranslate(Translate.language, {}, transport.type)} ${transport.type === 'minibus' || transport.type === 'truck' ? SetNativeTranslate(Translate.language, {}, transport.load_capacity) : ''}`}</div>
-            
-            <div className='ad_transport_item_ad_text_container'>{transport.ad_text}</div>
+            <div className='ad_transport_item_ad_header_container'>{`${SetNativeTranslate(Translate.language, {}, transport.type)} ${transport.type === 'minibus' || transport.type === 'truck' ? `${SetNativeTranslate(Translate.language, {}, transport.load_capacity)} ${SetNativeTranslate(Translate.language, {}, transport.side_type)}` : ''}`}</div>
 
-            <div className='ad_transport_item_contacts_container'>                
+            <div className={`ad_transport_item_ad_text_container ${Setting.app_theme}`}>{transport.ad_text}</div>
+
+            <div className='ad_transport_equipment_container'>
+                <div className='ad_transport_equipment_row'>
+                    {transport.thermo_bag === true ? <div className='ad_transport_equipment_item'>{SetNativeTranslate(Translate.language, {}, 'thermo_bag')}</div> : <></>}
+                    {transport.thermo_van === true ? <div className='ad_transport_equipment_item'>{SetNativeTranslate(Translate.language, {}, 'thermo_van')}</div> : <></>}
+                    {transport.refrigerator_minus === true ? <div className='ad_transport_equipment_item'>{SetNativeTranslate(Translate.language, {}, 'refrigerator_minus')}</div> : <></>}
+                    {transport.refrigerator_plus === true ? <div className='ad_transport_equipment_item'>{SetNativeTranslate(Translate.language, {}, 'refrigerator_plus')}</div> : <></>}
+                    {transport.hydraulic_platform === true ? <div className='ad_transport_equipment_item'>{SetNativeTranslate(Translate.language, {}, 'hydraulic_platform')}</div> : <></>}
+                    {transport.side_loading === true ? <div className='ad_transport_equipment_item'>{SetNativeTranslate(Translate.language, {}, 'side_loading')}</div> : <></>}
+                    {transport.glass_stand === true ? <div className='ad_transport_equipment_item'>{SetNativeTranslate(Translate.language, {}, 'glass_stand')}</div> : <></>}
+                </div>
+            </div>
+
+
+            <div className='ad_transport_item_contacts_container'>
                 <div className='ad_transport_item_ad_text_action_container'>{ad_user.name}, {ad_user.city}</div>
                 <div className='ad_transport_item_ad_text_action_container'>{SetNativeTranslate(Translate.language, {
                     russian: [`${user && user.isAuth ? ad_user.phone : 'Авторизуйтесь,чтобы увидеть телефон'}`],
