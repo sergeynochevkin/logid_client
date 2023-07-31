16a7d5b3436957947d3b8242f05e2983471191d5
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-07-31 20:53:11 +0300

    ad transp

--- src/pages/main/AdTransportSection.js
+++ src/pages/main/AdTransportSection.js
@@ -8,17 +8,45 @@ import { SetNativeTranslate } from '../../modules/SetNativeTranslate'
 const AdTransportSection = observer(() => {
   const { Ad } = useContext(AdContext)
   const { Setting } = useContext(SettingContext)
-
+  const [transports, setTransports] = useState([])
+  const { height, width } = useWindowDimensions();
   const { Translate } = useContext(TranslateContext)
 
+  function transportAdSelect(length, i) {
+    let indexArray = []
+    let transportsArray = []
 
+    for (; i < length + 10; i++) {
+      let index = Math.floor(Math.random() * Ad.transports.length);
+      if (!indexArray.includes(index)) {
+        indexArray.push(index)
+        if (indexArray.length === length) {
+          break
+        }
+      }
+    }
+    for (const index of indexArray) {
+      transportsArray.push(Ad.transports[index])
+    }
+    setTransports(transportsArray)
+    indexArray = []
+    transportsArray = []
+  }
 
+  useEffect(() => {
+    setTimeout(() => {
+      transportAdSelect(width < 425 ? 2 : width < 770 ? 3 : 6, 0)
+    }, 500)
+    setInterval(() => {
+      transportAdSelect(width < 425 ? 2 : width < 770 ? 3 : 6, 0)
+    }, 10000)
+  }, [])
 
   return (
     <>
       <div className={`ad_transport_container ${Setting.app_theme}`} >
         <div className={`ad_transport_section`}>
-          {Ad.selected_transports.map(transport => <AdTransportItem key={transport.id} transport={transport} />)}
+          {transports.map(transport => <AdTransportItem key={transport.id} transport={transport} transports={transports} />)}
         </div>
         <div className='how_to_add_text_container'>
           {SetNativeTranslate(Translate.language,
