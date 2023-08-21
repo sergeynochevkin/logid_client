bcf6870c659b63a8cb32f9169255438500413bfe
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-08-21 16:06:13 +0300

    rr

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -448,7 +448,7 @@ const Main = observer(() => {
 
             <div className={`admin_console_container ${Setting.app_theme}`}>
 
-              {Management.visits ?
+              {Management.visits.toDay>0 ?
                 <>
                   <AdminConsoleItem plan={70} currentRate={Management.visits.toDay} comment={
                     SetNativeTranslate(Translate.language,
