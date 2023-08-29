8a6407e03458ee7d2520d72d5b848f1980434f5e
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-08-29 19:08:03 +0300

    build

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import { ADMIN_ROUTE, USER_ROUTE, LOGIN_ROUTE, MAIN_ROUTE, MANAGER_ROUTE, RECOVERY_ROUTE, REGISTRATION_ROUTE, BOARD_ROUTE, BOARD_ITEM_ROUTE } from "./utils/consts"
 // import Management from './pages/Management'
-// import Main from './pages/main/Main'
+import Main from './pages/main/Main'
 // import User from "./pages/User"
 // import Board from "./pages/board/Board"
 // import BoardItemPage from "./pages/board/BoardItemPage"
 import { Suspense, lazy } from "react"
 
-const Main = lazy(() => import('./pages/main/Main'));
+// const Main = lazy(() => import('./pages/main/Main'));
 const Board = lazy(() => import('./pages/board/Board'));
 const User = lazy(() => import('./pages/User'));
 const Management = lazy(() => import('./pages/Management'));
