c5c600d29ee156c18d3693c963cbb5d6c0d4dbf0
sergeynochevkin <sergey.nochevkin@gmail.com>
2023-08-29 18:28:03 +0300

    build

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import { ADMIN_ROUTE, USER_ROUTE, MAIN_ROUTE, MANAGER_ROUTE, BOARD_ROUTE, BOARD_ITEM_ROUTE } from "./utils/consts"
 // import Management from './pages/Management'
-// import Main from './pages/main/Main'
+import Main from './pages/main/Main'
 // import User from "./pages/User"
 // import Board from "./pages/board/Board"
 // import BoardItemPage from "./pages/board/BoardItemPage"
 import asyncComponent from "./components/AsyncComponent"
 
 const Management = asyncComponent(() => import("./pages/Management"));
-const Main = asyncComponent(() => import("./pages/main/Main"));
+// const Main = asyncComponent(() => import("./pages/main/Main"));
 const User = asyncComponent(() => import("./pages/User"));
 const Board = asyncComponent(() => import("./pages/board/Board"));
 const BoardItemPage = asyncComponent(() => import("./pages/board/BoardItemPage"));
